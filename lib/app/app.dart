b7cff587064b7bdaf3ab40ee1fb699d80b586ce0
Narek Yapunjyan <nynarek@gmail.com>
2024-01-27 15:09:31 +0400

    merge chat screen ui

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_mobile_app_foundation/app/app_bootstrapper.dart';
+import 'package:flutter_mobile_app_foundation/presentation/chat/bloc/chat_screen_bloc.dart';
 import 'package:flutter_mobile_app_foundation/presentation/chat/chat_screen.dart';
 import 'package:flutter_mobile_app_foundation/theming/index.dart';
 
@@ -20,10 +22,30 @@ class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: appTheme,
-      debugShowCheckedModeBanner: false,
-      home: const ChatScreen(),
+    return StreamBuilder<bool>(
+      stream: AppBootstrapper.instance.isInitializedStream,
+      builder: (context, snapshot) {
+        var isInitialized = false;
+
+        if (snapshot.data != null) {
+          isInitialized = snapshot.data ?? false;
+        }
+
+        if (!isInitialized) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return MaterialApp(
+          theme: appTheme,
+          debugShowCheckedModeBanner: false,
+          home: BlocProvider.value(
+            value: get<ChatScreenBloc>()..add(const ChatScreenEvent.getData()),
+            child: const TelegramChatScreen(),
+          ),
+        );
+      }
     );
   }
 }
--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -1,25 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobile_app_foundation/presentation/chat/widgets/chat_bottom_field.dart';
+import 'package:flutter_mobile_app_foundation/presentation/chat/widgets/chat_screen_app_bar.dart';
+import 'package:flutter_mobile_app_foundation/presentation/chat/widgets/message_widget.dart';
 
-class ChatScreen extends StatelessWidget {
-  const ChatScreen({
-    super.key,
-  });
+class TelegramChatScreen extends StatefulWidget {
+  const TelegramChatScreen({super.key});
+
+  @override
+  State<TelegramChatScreen> createState() => _TelegramChatScreenState();
+}
+
+class _TelegramChatScreenState extends State<TelegramChatScreen> {
+  late GlobalKey key;
+
+  @override
+  void initState() {
+    super.initState();
+    key = GlobalKey();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          ChatBottomField(
-            onFileIconTap: () {},
-            onStickerIconTap: () {},
-            onEmojiIconTap: () {},
-            onVoiceIconTap: () {},
-            onSendIconTap: () {},
+    return SafeArea(
+      child: Scaffold(
+        appBar: const PreferredSize(
+          preferredSize: Size.fromHeight(50),
+          child: ChatScreenAppBar(
+              userName: 'Narek',
+              seenMessage: 'last seen recently',
+              imagePath: ''),
+        ),
+        bottomSheet: ChatBottomField(
+          onFileIconTap: () {},
+          onStickerIconTap: () {},
+          onEmojiIconTap: () {},
+          onVoiceIconTap: () {},
+          onSendIconTap: () {},
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                'https://w0.peakpx.com/wallpaper/220/854/HD-wallpaper-whatsapp-black-abstract-abstract-digital-abstraction-abstracts-background-digital-pattern-texture.jpg',
+              ),
+            ),
           ),
-        ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                child: const MessageWidget(
+                  isMy: true,
+                  message:
+                      'Hdsacdsvcdadsgsdfgfdgdsgfgfdfgsdfgsfdsfgsgafadsagergregwfdfadsfsdfsdfaff',
+                  isUserSaw: true,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
